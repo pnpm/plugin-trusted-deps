1bbabb75f0bf861e0a9232263eee5d1ed2517859
Zoltan Kochan <z@kochan.io>
2025-12-31 15:16:41 +0100

    fix: do not reenable explicitly disallowed dependencies

--- pnpmfile.cjs
+++ pnpmfile.cjs
@@ -4,7 +4,17 @@ module.exports = {
       if (config.onlyBuiltDependencies == null) {
         config.onlyBuiltDependencies = []
       }
-      config.onlyBuiltDependencies.push(...require('./allow.json'))
+      const defaultAllowed = require('./allow.json')
+      if (!config.ignoredBuiltDependencies?.length) {
+        config.onlyBuiltDependencies.push(...defaultAllowed)
+      } else {
+        const ignored = new Set(config.ignoredBuiltDependencies)
+        for (const allowed of defaultAllowed) {
+          if (!ignored.has(allowed)) {
+            config.onlyBuiltDependencies.push(allowed)
+          }
+        }
+      }
       return config
     },
   },
