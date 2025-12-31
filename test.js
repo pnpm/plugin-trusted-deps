e72917519d7c5613c0707cb8ec0e1f8c68d85e75
Zoltan Kochan <z@kochan.io>
2025-12-31 13:33:18 +0100

    fix: do not reenable explicitly disallowed dependencies

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
+const { test } = require('node:test')
 const assert = require('assert')
 const { TRUSTED_PACKAGE_NAMES } = require('./index.js')
+const pnpmfile = require('./pnpmfile.cjs')
 
-assert(Array.isArray(TRUSTED_PACKAGE_NAMES))
-assert.equal(typeof TRUSTED_PACKAGE_NAMES[0], 'string')
+test('TRUSTED_PACKAGE_NAMES', () => {
+  assert(Array.isArray(TRUSTED_PACKAGE_NAMES))
+  assert.equal(typeof TRUSTED_PACKAGE_NAMES[0], 'string')
+})
+
+test('do not reenable dependency builds', () => {
+  const config =  {
+    ignoredBuiltDependencies: ['esbuild'],
+  }
+  pnpmfile.hooks.updateConfig(config)
+  assert(!config.onlyBuiltDependencies.includes('esbuild'))
+  assert(config.onlyBuiltDependencies.includes('@apollo/rover'))
+})
