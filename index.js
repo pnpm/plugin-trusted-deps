e4ca3b7d90787869b5d1c05d01dfa5716e08e144
Zoltan Kochan <z@kochan.io>
2026-03-23 11:26:12 +0100

    fix: lazy-load allowBuilds.json so tests work without generating it

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 module.exports.TRUSTED_PACKAGE_NAMES = require('./allow.json')
-module.exports.DEFAULT_ALLOW_BUILDS = require('./allowBuilds.json')
+
+Object.defineProperty(module.exports, 'DEFAULT_ALLOW_BUILDS', {
+  get () { return require('./allowBuilds.json') },
+  enumerable: true,
+})
